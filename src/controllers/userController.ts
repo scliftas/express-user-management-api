4317efe0fab060438b67874e444546c39e6b1906
scliftas <scliftas@gmail.com>
2019-07-03 18:50:44 +0100

    Fixed typos in user controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import UserService from '../services/userService';
 
-export default class JobController {
+export default class UserController {
     protected service: UserService;
 
     constructor() {
@@ -14,21 +14,21 @@ export default class JobController {
     }
 
     async create(req: Request, res: Response) {
-        const job = await this.service.create(req.body);
+        const result = await this.service.create(req.body);
     
-        res.status(200).json(job);
+        res.status(200).json(result);
     }
 
     async update(req: Request, res: Response) {
-        const job = await this.service.update(req.params.id, req.body);
+        const result = await this.service.update(req.params.id, req.body);
 
-        res.status(200).json(job);
+        res.status(200).json(result);
     }
 
     async get(req: Request, res: Response) {
-        const job = await this.service.get(req.params.id);
+        const result = await this.service.get(req.params.id);
 
-        res.status(200).json(job);
+        res.status(200).json(result);
     }
 
     async delete(req: Request, res: Response) {
