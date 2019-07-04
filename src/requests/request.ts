fc83aac9630cbcb98ffdd82845f6b6c822203016
scliftas <scliftas@gmail.com>
2019-07-04 19:42:54 +0100

    Added comments to request class

--- src/requests/request.ts
+++ src/requests/request.ts
@@ -1,9 +1,19 @@
 import joiValidator from 'express-joi-validation';
 
+/**
+ * Custom request pattern written to make writing Joi
+ * validation a bit cleaner. Expects a rules property defining
+ * all validation rules for either the request parameters and/or
+ * the request body.
+ */
 export default abstract class Request {
-    protected static type: any;
     protected static rules: any;
 
+    /**
+     * Runs Joi validation against the defined rules property
+     * 
+     * @returns Joi validation results
+     */
     static validate() {
         const validator = joiValidator({});
 
