68d8d54243c445be77f001a43ea0b92a9e4ac6b4
scliftas <scliftas@gmail.com>
2019-07-05 15:09:25 +0100

    Wrapped all controller methods in catcher higher order function

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -16,26 +16,26 @@ export default class UserController {
     constructor() {
         this.service = new UserService();
 
-        this.create = this.create.bind(this);
-        this.update = this.update.bind(this);
-        this.get = this.get.bind(this);
+        this.create = catcher(this.create.bind(this));
+        this.update = catcher(this.update.bind(this));
+        this.get = catcher(this.get.bind(this));
         this.getOne = catcher(this.getOne.bind(this));
-        this.delete = this.delete.bind(this);
+        this.delete = catcher(this.delete.bind(this));
     }
 
-    async create(req: Request, res: Response) {
+    async create(req: Request, res: Response, next: Function) {
         const result = await this.service.create(req.body);
     
         res.status(200).json(result);
     }
 
-    async update(req: Request, res: Response) {
+    async update(req: Request, res: Response, next: Function) {
         const result = await this.service.update(req.params.id, req.body);
 
         res.status(200).json(result);
     }
 
-    async get(req: Request, res: Response, next) {
+    async get(req: Request, res: Response, next: Function) {
         const result = await this.service.get();
 
         res.status(200).json(result);
@@ -47,7 +47,7 @@ export default class UserController {
         res.status(200).json(result);
     }
 
-    async delete(req: Request, res: Response) {
+    async delete(req: Request, res: Response, next: Function) {
         const result = await this.service.delete(req.params.id);
 
         res.status(200).json(result);
